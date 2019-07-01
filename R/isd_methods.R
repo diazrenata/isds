4457510612a74931c78e0d53efe005e801bb9b0e
Renata Diaz <diaz.renatam@gmail.com>
2019-07-01 10:33:24 -0400

    starting to document

--- R/isd_methods.R
+++ R/isd_methods.R
@@ -1,5 +1,18 @@
-# Integrate density of GMM
-get_integrated_density <- function(gmm, interval_size = 0.0001, min_size = 0, max_size = 8, type = "sim", dat_name, stdev) {
+#' Integrate density based on GMM
+#' @param gmm result of `fit_gmm`
+#' @param interval_size width, in log units, of intervals to integrate over
+#' @param min_size minimum of window to integrate - v. v. small
+#' @param max_size maximum of window to integrate - v. v. large compared to rodents
+#' @param type "sim" or "empirical", for collecting results later
+#' @param dat_name source dataset, for collecting results later
+#' @param stdev if sim, standard deviation of intraspecific body size used to generate sim
+#'
+#' @return dataframe with density, turnpoints marked
+#' @export
+#' @importFrom stats predict
+#' @importFrom dplyr mutate
+#' @importFrom pastecs turnpoints
+get_integrated_density <- function(gmm, interval_size = 0.0001, min_size = 0, max_size = 8, type = "sim", dat_name, stdev = NA) {
 
   density_evalpoints <- seq(min_size, max_size, by = interval_size)
 
@@ -38,128 +51,4 @@ get_integrated_density <- function(gmm, interval_size = 0.0001, min_size = 0, ma
   integrated_density$stdev = stdev
 
   return(integrated_density)
-}
-
-
-# Find troughs in density to a given threshold
-find_pit_sections <- function(pit, integrated_density, threshold) {
-
-  turns <- integrated_density %>%
-    dplyr::filter(start_is_turnpoint) %>%
-    dplyr::mutate(start_is_peak = by_max >= threshold)
-
-  lower_peak <- turns %>%
-    dplyr::filter(start_is_peak, start <= pit) %>%
-    dplyr::filter(start == max(start))
-
-  upper_peak <- turns %>%
-    dplyr::filter(start_is_peak, start >= pit) %>%
-    dplyr::filter(start == min(start))
-
-  pit_subset <- integrated_density %>%
-    dplyr::filter(dplyr::between(start, lower_peak$start[1], upper_peak$start[1]))
-
-  pit_below_threshold <- pit_subset %>%
-    dplyr::filter(by_max <= threshold)
-
-  return(pit_below_threshold)
-}
-
-find_all_pits <- function(integrated_density, threshold = 0.05) {
-
-  peaks_above_thresh <- dplyr::filter(integrated_density,
-                                      start_is_peak, by_max >= threshold)
-
-  pits <- integrated_density %>%
-    dplyr::filter(start_is_pit, by_max <= threshold,
-                  start > min(peaks_above_thresh$start))
-
-  if(!any(pits$by_max <= threshold)) {
-    return(NULL)
-  }
-
-  all_pits <- lapply(pits$start, FUN = find_pit_sections,
-                     integrated_density = integrated_density,
-                     threshold = threshold)
-  return(all_pits)
-}
-
-find_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
-  all_pits <- find_all_pits(integrated_density = integrated_density,
-                            threshold = threshold)
-  if(is.null(all_pits)) {
-    warning("No troughs below threshold")
-    return(NULL)
-  }
-
-  pit_boundaries <- data.frame(
-    pit_index = 1:length(all_pits),
-    pit_start = NA,
-    pit_stop = NA,
-    pit_length = NA
-  )
-
-  for(i in 1:nrow(pit_boundaries)) {
-    pit_boundaries$pit_start[i] <- min(all_pits[[i]]$start)
-    pit_boundaries$pit_stop[i] <- max(all_pits[[i]]$stop)
-    pit_boundaries$pit_length[i] <- pit_boundaries$pit_stop[i] - pit_boundaries$pit_start[i]
-  }
-  return(pit_boundaries)
-}
-
-add_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
-  peaks_above_thresh <- dplyr::filter(integrated_density,
-                                      start_is_peak == TRUE,
-                                      by_max >= threshold)
-
-  if(nrow(peaks_above_thresh) < 2) {
-    integrated_density$start_is_trough_start <- FALSE
-    integrated_density$start_is_trough_stop <- FALSE
-    integrated_density$is_in_trough <- FALSE
-    warning("Fewer than 2 peaks above threshold")
-    return(integrated_density)
-  }
-  pit_boundaries <- find_all_pit_boundaries(integrated_density = integrated_density,
-                                            threshold = threshold)
-  if(is.null(pit_boundaries)) {
-    integrated_density$start_is_trough_start <- FALSE
-    integrated_density$start_is_trough_stop <- FALSE
-    integrated_density$is_in_trough <- FALSE
-    return(integrated_density)
-  }
-
-  integrated_density$start_is_trough_start <- integrated_density$start %in% pit_boundaries$pit_start
-  integrated_density$start_is_trough_stop <- integrated_density$start %in% pit_boundaries$pit_stop
-  integrated_density$is_in_trough <- FALSE
-  for(i in 1:nrow(pit_boundaries)) {
-    integrated_density$is_in_trough[ which(dplyr::between(integrated_density$start,
-                                                          pit_boundaries$pit_start[i],
-                                                          pit_boundaries$pit_stop[i]))] <- TRUE
-  }
-
-  return(integrated_density)
-}
-
-# Count modes and troughs in density
-
-isd_methods_summary <- function(integrated_density, threshold, empirical = F) {
-  nmodes  <- sum(integrated_density$start_is_peak)
-
-  nmodes_above_threshold <- nrow(dplyr::filter(integrated_density,
-                                               start_is_peak == TRUE,
-                                               by_max >= threshold))
-
-  ntroughs <- sum(integrated_density$start_is_trough_start)
-
-  ntroughs_possible <- nmodes_above_threshold - 1
-
-  summary_variables <- list(nmodes = nmodes,
-                            nmodes_above_threshold = nmodes_above_threshold,
-                            ntroughs = ntroughs,
-                            ntroughs_possible = ntroughs_possible,
-                            threshold = threshold,
-                            empirical = empirical)
-
-
-  return(summary_variables)
 }
--- R/threshold_methods.R
+++ R/threshold_methods.R
@@ -1,14 +1,28 @@
+#' Find gaps to a given threshold
+#'
+#' Cuts integrated density off at a given probability and finds gaps in the remaining distribution. (Gaps defined as non-sequential starting values, as would occur if the intervening values got filtered out when the distribution was cut off).
+#'
+#' @param threshold cutoff probability for gaps
+#' @param integrated_density result of get_integrated_density
+#'
+#' @return integrated_density with columns for `is_gap_start`, `is_gap_end`
+#' @export
+#' @importFrom dplyr lag lead filter left_join
+#' @importFrom tidyr replace_na
 find_gaps <- function(threshold, integrated_density) {
-  first_peak <- min(integrated_density[ which(integrated_density$start_is_peak), "start"])
-  
-  within_peaks <- dplyr::filter(integrated_density, start >= first_peak, by_max >= threshold) %>%
-    dplyr::mutate(start_diff = (start - dplyr::lag(start, n = 1, default = NA)) > 0.000100000001) %>%
+  first_peak <- min(integrated_density[
+    which(integrated_density$start_is_peak), "start"])
+
+  within_peaks <- dplyr::filter(integrated_density, start >= first_peak,
+                                by_max >= threshold) %>%
+    dplyr::mutate(start_diff =
+                    (start - dplyr::lag(start, n = 1, default = NA)) > 0.000000001) %>%
     dplyr::mutate(is_gap_start = dplyr::lead(start_diff, n = 1, default = F),
                   is_gap_end = start_diff) %>%
     tidyr::replace_na(replace = list(start_diff = FALSE,
                                      is_gap_start = FALSE,
                                      is_gap_end = FALSE))
-  
+
   integrated_density <- dplyr::left_join(integrated_density,
                                          within_peaks,
                                          by = colnames(integrated_density)) %>%
@@ -16,35 +30,82 @@ find_gaps <- function(threshold, integrated_density) {
                                      is_gap_start = FALSE,
                                      is_gap_end = FALSE)) %>%
     dplyr::mutate(threshold = threshold)
-  
+
   return(integrated_density)
-  
+
 }
 
+#' Count gaps
+#'
+#' @param integrated_density result of find_gaps(integrated_density)
+#'
+#' @return number of gaps
+#' @export
 count_gaps <- function(integrated_density) {
   return(length(which(integrated_density$is_gap_start)))
 }
 
+#' Count peaks
+#'
+#' @param integrated_density result of find_gaps(integrated_density).
+#'
+#' @return number of peaks
 count_peaks <- function(integrated_density) {
-  return(length(which(integrated_density$start_is_peak)))
+ return(length(which(integrated_density$start_is_peak)))
 }
 
+#' Get threshold
+#'
+#' Retrieve the threshold probability used in find_gaps. For collecting results.
+#'
+#' @param integrated_density result of find_gaps(integrated_density)
+#'
+#' @return threshold value
+#' @export
 get_threshold <- function(integrated_density) {
   return(integrated_density$threshold[1])
 }
 
+#' Get type
+#' For collecting results.
+#' @param integrated_density result of find_gaps(integrated_density)
+#'
+#' @return "emp" or "sim"
+#' @export
 get_type <- function(integrated_density) {
   return(integrated_density$type[1])
 }
+
+#' Get dataset name
+#' For collecting results.
+#' @param integrated_density result of find_gaps(integrated_density)
+#'
+#' @return Name of dataset used in integrated_density/sims
+#' @export dat_name
 
 get_datname <- function(integrated_density) {
   return(integrated_density$dat_name[1])
 }
 
+#' Retrieve standard deviation
+#'
+#' Retrieve sd used to create ISD for sims. For collecting results.
+#'
+#' @param integrated_density result of find_gaps(integrated_density)
+#'
+#' @return sd
+#' @export
 get_stdev <- function(integrated_density) {
   return(integrated_density$stdev[1])
 }
 
+#' Collect results
+#'
+#' @param integrated_density Result of find_gaps
+#'
+#' @return data frame 1 row long with summary of thresholding exercise.
+#' @export
+#'
 get_result <- function(integrated_density) {
   result <- data.frame(ngaps = count_gaps(integrated_density),
                        npeaks = count_peaks(integrated_density),
