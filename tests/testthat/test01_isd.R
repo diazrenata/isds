a28630dccb310b4ea4ea09b638128189600126e3
Renata Diaz <diaz.renatam@gmail.com>
2019-07-05 12:30:49 -0400

    fix namespace, tests

--- tests/testthat/test01_isd.R
+++ tests/testthat/test01_isd.R
@@ -1,15 +1,13 @@
 context("Simple first test")
 
-dat1 <- neonbecs::get_toy_portal_data()
+dat1 <- get_toy_portal_data()
 
 test_that("making an ISD works", {
 
   isd <-dat1 %>%
-    replicatebecs::add_energy_sizeclass() %>%
-    neonbecs::make_isd()
+    make_isd()
 
   expect_true(is.data.frame(isd))
   expect_false(anyNA(isd))
-  expect_true(colnames(isd)[7] == "ln_energy")
 
 })
