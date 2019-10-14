20c4b9958718c1145382fa35ad095be8cd80e475
Renata Diaz <diaz.renatam@gmail.com>
2019-10-14 14:58:54 -0400

    clean out everything

--- get_cache.sh
+++
@@ -1,4 +0,0 @@
-rm drake/*
-
-rsync -av diaz.renata@rsync.rc.ufl.edu:/home/diaz.renata/isds/drake/drake-cache.sqlite drake/
-rsync -av diaz.renata@rsync.rc.ufl.edu:/home/diaz.renata/isds/drake/cache_log.txt drake/
