4c87374c0bd49996240eeb463fbf378257de7238
Renata Diaz <diaz.renatam@gmail.com>
2019-12-09 13:06:17 -0500

    begin functionifying overlap sampling, overlap explainer report

---
+++ R/sample_overlap_scenarios.R
@@ -0,0 +1,97 @@
+#' Sample a community
+#'
+#' @param means Vector of species means
+#' @param sds Vector of standard deviations
+#' @param abunds Vector of abundanaces
+#'
+#' @return data frame of simulated community
+#' @export
+#' @importFrom dplyr bind_rows
+#'
+sample_community <- function(means, sds = NULL, abunds) {
+
+  if(is.null(sds)) {
+    sds <- .15 * means
+  }
+
+  comm <- list()
+
+  for(i in 1:length(means)) {
+    comm[[i]] <- data.frame(
+      species = i,
+      wgt = rnorm(mean = means[i], sd = sds[i], n = abunds[i])
+    )
+  }
+
+  comm <- dplyr::bind_rows(comm)
+
+  return(comm)
+}
+
+#' Plot an isd
+#'
+#' @param community_df df with wgt
+#'
+#' @return plot of isd
+#' @export
+#' @importFrom ggplot2 ggplot geom_density theme_bw xlim
+plot_isd <- function(community_df) {
+
+  xlims <- c(0, 1.5 * max(community_df$wgt))
+
+  isd_plot <- ggplot(data = community_df, aes(x = wgt)) +
+    geom_density() +
+    theme_bw() +
+    xlim(xlims)
+
+  return(isd_plot)
+
+}
+
+#' Plot species body size distributions
+#'
+#' @param community_df df with species, wgt
+#'
+#' @return sbsd plot
+#' @export
+#' @importFrom ggplot2 ggplot geom_density theme_bw scale_color_viridis_d scale_fill_viridis_d xlim theme
+plot_sbsds <- function(community_df) {
+
+  community_df$species <- as.factor(community_df$species)
+  xlims <- c(0, 1.5 * max(community_df$wgt))
+
+  sbsd_plot <- ggplot(data = community_df, aes(x = wgt, group = species, color = species, fill = species)) +
+    geom_density(alpha = .3) +
+    theme_bw() +
+    scale_color_viridis_d(option = "plasma", end = .8) +
+    scale_fill_viridis_d(option = "plasma", end = .8)+
+    xlim(xlims) +
+    theme(legend.position = "none")
+
+  return(sbsd_plot)
+
+}
+
+#' Plot histogram of overlap values
+#'
+#' @param overlap_df df with overlap, optionally a column to weight by
+#' @param weighting_col optional name of column to weight by
+#'
+#' @return histogram
+#' @export
+#' @importFrom ggplot2 ggplot geom_histogram xlim geom_vline theme_bw
+plot_overlaps <- function(overlap_df, weighting_col = NULL) {
+
+  if(!is.null(weighting_col)) {
+    overlap_df <- data.frame(
+      overlap = rep(overlap_df$overlap, times = overlap_df[, weighting_col])
+    )
+  }
+
+  overlap_plot <- ggplot(data = overlap_df, aes(x = overlap)) +
+    geom_histogram(boundary = 0, closed = "left", binwidth = .1) +
+    xlim(-.2, 1.2) +
+    geom_vline(xintercept = c(0, 1), color = "red") +
+    theme_bw()
+
+}
---
+++ analysis/overlap_explainer.md
@@ -0,0 +1,41 @@
+Measuring overlap in SBSDs
+================
+
+SBSD = species body size distribution. The frequency/probability density of body sizes across all individuals of a species.
+
+Rationale & background of using overlap
+---------------------------------------
+
+The ISD (the size distribution across all individuals of all species) is...slippery and often subject to interpretation.
+
+Both Holling and ESS would predict polarized/bimodal distribution of overlap among all pairings of species. Species should either be quite similar or quite different, so high or low, but not intermediate, overlap.
+
+I've imported the SBSD overlap metric from [Read et al 2018](https://onlinelibrary.wiley.com/doi/full/10.1111/ecog.03641):
+
+1.  For all pairs of species in a community, construct the KDEs across the entire body size range and standardize so each KDE integrates to 1.
+2.  Integrate the minimum of the two KDEs at each evaluation point.
+
+In principle this varies from 0 to 1 for each pair of species.
+
+Foreshadowing artefacts from rare species
+-----------------------------------------
+
+1.  Pointy KDEs
+2.  Higher uncertainty wrt the true SBSD mean, sd, and indeed entire distribution.
+3.  We may inherently want to downweight rare species, but *this is separate from* the above.
+
+Demo with simple cases
+----------------------
+
+Let's leave rare species aside for the moment, and focus on how we expect the overlap distributions to come out under a few toy scenarios:
+
+1.  All species are essentially the same; complete overlap
+2.  Two clusters of species. Within clusters, species are essentially the same. The clusters do not overlap.
+3.  All species are distinct and do not overlap.
+4.  All species are different but overlap significantly.
+
+For now, we will give all species an equal, reasonably large, number of individuals (50). This is wildly unrealistic for real communities.
+
+We will assume all species' SBSDs are normal distributions, and that the standard deviation scales with the mean according to some coefficient. `.15` seems like a plausible coefficient to me.
+
+![](overlap_explainer_files/figure-markdown_github/show%20plots-1.png)![](overlap_explainer_files/figure-markdown_github/show%20plots-2.png)![](overlap_explainer_files/figure-markdown_github/show%20plots-3.png)![](overlap_explainer_files/figure-markdown_github/show%20plots-4.png)
